3167e86ac64800e7b983ebd736a5a1990b9a82df
Iryna <kytsenkoiryna33@gmail.com>
2025-11-03 00:27:59 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Flower
+{
+    public void Accept(Action<Flower> visitor) => visitor(this);
+}
+
+class Program
+{
+    static void Main()
+    {
+        var flower = new Flower();
+        flower.Accept(f => Console.WriteLine("Перевірено якість квітки "));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
